a445181f62d6c497d97b25cb941482f243750c3f
Sabrina Almeida <109253438+sabrinaalh@users.noreply.github.com>
2022-08-20 20:33:16 -0300

    upaupa

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+//console.log('O console funcionou perfeitamente.')
+
+var vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){ //Condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+console.log('Digija sempre usando o cinto de segurança.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Eua'
+console.log(`Vivendo em ${país}:`)
+if (país != 'Brasil'){
+    console.log('Você é Estrangeiro!')
+}else{
+    console.log('Você é Brasileiro meu mano.')
+}
